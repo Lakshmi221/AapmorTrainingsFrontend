9de196de3ca4740421ce935893179d4e7885f228
AamorAccount <blakshmi@aapmor.com>
2025-08-18 18:20:56 +0530

    Update webapp.config.js

--- webapp.config.js
+++ webapp.config.js
@@ -1,14 +1,22 @@
+// {
+//   "navigationFallback": {
+//     "rewrite": "/index.html",
+//     "exclude": ["/static/*", "/*.{png,jpg,gif,ico,css,js}"]
+//   },
+//   "auth": {
+//     "routes": [
+//       {
+//         "route": "/auth-callback",
+//         "rewrite": "/index.html"
+//       }
+//     ]
+//   }
+// }
+
+
 {
   "navigationFallback": {
     "rewrite": "/index.html",
-    "exclude": ["/static/*", "/*.{png,jpg,gif,ico,css,js}"]
-  },
-  "auth": {
-    "routes": [
-      {
-        "route": "/auth-callback",
-        "rewrite": "/index.html"
-      }
-    ]
+    "exclude": ["/images/*", "/static/*"]
   }
 }
